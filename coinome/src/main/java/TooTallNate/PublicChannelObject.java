827f28268e4194e51636993ced4b8bffec856752
Prabhjyot Singh <prabhjyotsingh@gmail.com>
2017-12-23 09:47:25 +0530

    hello world

---
+++ coinome/src/main/java/TooTallNate/PublicChannelObject.java
@@ -0,0 +1,27 @@
+package TooTallNate;
+
+import java.util.ArrayList;
+
+public class PublicChannelObject {
+
+  public String getIdentifier() {
+    return identifier;
+  }
+
+  public void setIdentifier(String identifier) {
+    this.identifier = identifier;
+  }
+
+  public ArrayList getMessage() {
+    return message;
+  }
+
+  public void setMessage(ArrayList message) {
+    this.message = message;
+  }
+
+  String identifier;
+  ArrayList message;
+
+
+}
